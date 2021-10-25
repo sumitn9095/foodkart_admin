9e18e944760f349270521abead642ac6f0f4c8a8
Sumit <sumit.nirgude@gmail.com>
2021-10-25 15:49:59 +0000

    server updates

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'foodkart'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
+        database: env('DATABASE_NAME', 'sumitfnn_foodkart_admin'),
+        username: env('DATABASE_USERNAME', 'sumitfnn_foodkart'),
+        password: env('DATABASE_PASSWORD', '#SN85951055'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
