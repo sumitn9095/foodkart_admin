6f6b760c98958fa871c2ac152d7830c8f72f8727
Sumit <sumit.nirgude@gmail.com>
2021-10-26 16:38:07 +0530

    testing...

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env("APP_HOST", "162.241.85.73"),
-  port: env.int("NODE_PORT", 1337),
+  host: env("APP_HOST", "http://sumit.digital"),
+  port: env.int("NODE_PORT", 5600),
 });
